a49969ae2f162fab3cb3cdcd50f5a0020468556e
Alex Nichol <unixpickle@gmail.com>
2014-09-27 08:07:27 -0400

    added benchmark

---
+++ README.md
@@ -0,0 +1,5 @@
+# Benchmarks
+
+Using [randomlist.txt](randomlist.txt), this program found the social network of the word "foo" in 30 seconds using a single core af an Intel Core i7 2.6 GHz.
+
+Perhaps this was not the most efficient solution...
--- bin/social_words.dart
+++ bin/social_words.dart
@@ -8,16 +8,18 @@ part 'src/search.dart';
 part 'src/levenshtein_dictionary.dart';
 
 void main(List<String> args) {
-  if (args.length != 2) {
+  if (args.length != 3) {
     stderr.writeln('Usage: dart social_words.dart <start word> '
-        '<path to dictionary>');
+        '<path to dictionary> <output file>');
     exit(1);
   }
   var startWord = args[0];
   var dictionary = args[1];
+  var outputFile = args[2];
   new Search(dictionary, startWord).run().then((words) {
-    for (var word in words) {
-      print(word);
-    }
+    return new File(outputFile).writeAsString(words.join('\n'));
+  }).catchError((e) {
+    stderr.writeln('Error: $e');
+    exit(1);
   });
 }
